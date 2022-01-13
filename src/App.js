d42cb0ca9eb1d20b686f3856dfd0105159c2d7ba
divyadima25 <61356762+divyadima25@users.noreply.github.com>
2022-01-13 12:05:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello world! I am master and I am working fine!!
+            Hello world! I am master and this is from freestyle project!!
           </p>
           <a
             className="App-link"
